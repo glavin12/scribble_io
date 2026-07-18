655dc203c32377f8cb3f69a6e77dc40cad166913
glavin12 <bhagyparmar727@gmail.com>
2026-07-18 21:29:15 +0530

    there was a problem around that we are gettin a 404 error wehn we relaod the web page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './App.css'
 import App from './App.jsx'
 
+// ponytail: HashRouter so page reloads never 404 on Render static sites
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
